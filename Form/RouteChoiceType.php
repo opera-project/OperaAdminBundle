741e038410e764c0d8e71d51f073f3220c55b987
Nsy <noelie@donkeycode.com>
2019-01-17 13:05:15 +0100

    add Route choice type for page form

---
+++ Form/RouteChoiceType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Opera\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RouteChoiceType extends AbstractType
+{
+    private $routes;
+
+    public function __construct(RouterInterface $router)
+    {
+        foreach ($router->getRouteCollection()->all() as $routeName => $route) {
+            if ($route->getDefault("_controller")) {
+                $controllerName = $this->getControllerName($route->getDefault("_controller"));
+                if ($controllerName) {
+                    $this->routes[$controllerName][$routeName] = $routeName;
+                } else {
+                    $this->routes["Other"][$routeName] = $routeName;
+                }
+            }
+
+        }
+    }
+
+    /**
+     * ex:
+     *  input -> "Opera\AdminBundle\Controller\AdminPagesController::layout"
+     *  output -> "AdminPagesController"
+     */
+    private function getControllerName(string $controllerRoutePath)
+    {
+        preg_match('/.*\\\\(.*)::.*/', $controllerRoutePath, $matches);
+
+        if (isset($matches[1])) {
+            return $matches[1];
+        }
+
+        return null;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => $this->routes,
+        ]);
+    }
+
+    public function getParent()
+    {
+        return \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class;
+    }
+}
